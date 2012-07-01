77933cbffd2c0cd2e32218c8579aafe2d20654a1
Nathan Campos <eeepc904@gmail.com>
2012-07-01 16:16:42 -0300

    Started a test project for the PhoneGap library

---
+++ phonegap/test project/GOAH-M Test/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  GOAH-M Test
+//
+//  Created by Nathan Campos on 7/1/12.
+//  Copyright Paulino 2012. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"AppDelegate");
+    [pool release];
+    return retVal;
+}
